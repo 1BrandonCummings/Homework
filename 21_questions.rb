2aafb3e081b55ae28ebb38551f1486cc9da3b9df
1BrandonCummings <brandoncummingsfl@gmail.com>
2014-08-02 16:04:58 -0400

    Completed basic 21 question functionality

--- 21_questions.rb
+++ 21_questions.rb
@@ -16,47 +16,59 @@
 
 
 
-question_1 = "Do we ever see the darkside of the moon? (Yes or No)"					#no
-question_2 = "Does OOP stand for Optical Oriented Programming? (Yes or No)"			#no
-question_3 = "Does a Giraffe have as many neck bones as a human? (Yes or No)"		#yes
-question_4 = "Does a horse have more than 1 stomach? (Yes or No)" 					#no
-question_5 = "Does Roger Federer have more than 7 Wimbledon Titles? (Yes or No)" 	#yes
-
+question_1 = "Do we ever see the darkside of the moon? (Yes or No)"					
 answer_1 = "no"
+
+question_2 = "Does OOP stand for Optical Oriented Programming? (Yes or No)"			
 answer_2 = "no"
+
+
+question_3 = "Does a Giraffe have as many neck bones as a human? (Yes or No)"		
 answer_3 = "yes"
+
+
+question_4 = "Does a horse have more than 1 stomach? (Yes or No)" 					
 answer_4 = "no"
+
+
+question_5 = "Does Roger Federer have more than 7 Wimbledon Titles? (Yes or No)" 	
 answer_5 = "yes"
 
 
-questions = [question_1, question_2, question_3, question_4, question_5]
-answers = [answer_1, answer_2, answer_3, answer_4, answer_5] 
-counter = 0
+
 score = 0
+final_tally = 0
+pairs = [[question_1, answer_1], [question_2, answer_2],[question_3,answer_3],[question_4,answer_4],[question_5,answer_5]]
 
 
+pairs.each do |pair|
 
+		puts pair[0]
+		user_input = gets.chomp
 
- questions.each do |question|
-	puts question 
-	user_input = gets.chomp.to_s
-	answer_to_question = user_input
+			
+				if user_input.downcase == pair[1]
+					score += 1
+					final_tally += 1
+					
+				elsif user_input.downcase != pair[1]
+					score -= 1
+					
 
-	if answer_to_question ==  answers[0]
+				end	
+	
+end
 
-		
-    puts "Equals"
-		
-
-	elsif answer_to_question != answers[counter]
-	 puts "Please type Yes or No"
-
-	end
-
+if score > 0
+	puts "You win!"
+	puts "With a score of #{final_tally} out of 5"
+else puts "You lose!"
+	puts "With a score of #{final_tally} out of 5"
 end
 
 
-puts score
+
+
 
 
 
